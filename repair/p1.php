723e8b4739e731a7ab5fb07b3f210fcde855c292
莊鈺婷 <abc50931@hotmail.com>
2013-01-05 00:44:40 +0800

    新增報修資料夾
    上傳三個檔案
    p1.php
    p2.php
    p3.php

---
+++ repair/p1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>報修</title>
+	</head>
+	<body>
+		<center>
+			<div class=out1 style='text-align:center; line-height:450px'>
+				<BUTTON onclick="self.location.href='p2.php'">填 報 修 單</BUTTON>
+				<BUTTON onclick="self.location.href='p4.php'">改 報 修 單</BUTTON>
+			</div>
+       	<center/>
+	</body>
+</html>
