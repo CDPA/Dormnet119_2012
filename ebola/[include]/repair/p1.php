f220887cd24025e5dc7ef988eaee8d2c97cef217
莊鈺婷 <abc50931@hotmail.com>
2013-01-11 23:55:58 +0800

    include以詳寫的框架
    快要十二點了~~~

--- ebola/[include]/repair/p1.php
+++ ebola/[include]/repair/p1.php
@@ -1,3 +1,18 @@
-
+﻿
 <?php	// [Info.] The following code will be included in main page : div class "content"
 ?>
+
+
+		<center>
+			<div>			
+				<BUTTON onclick="self.location.href='p1.php'">報修</BUTTON>
+				<BUTTON onclick="self.location.href='p2.php'">前置檢查</BUTTON>
+				<BUTTON onclick="self.location.href='p3.php'">填報修單</BUTTON>
+				<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
+			</div>
+			<hr>
+			<div class=out1 style='text-align:center; line-height:450px'>
+				<BUTTON onclick="self.location.href='p2.php'">填報修單</BUTTON>
+				<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
+			</div>
+       	<center/>
