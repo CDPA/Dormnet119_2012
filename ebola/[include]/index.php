42729822ea06fe6e8b08a5ebfa75720cc8d1d23d
Ebola <ebola777@yahoo.com.tw>
2013-01-11 22:44:22 +0800

    Ebola's 1st dormnet UI
    DO NOT ADD ANY STUFF IN THIS FOLDER
    OR I WILL SCREW YOU UP

---
+++ ebola/[include]/index.php
@@ -0,0 +1,20 @@
+
+<?php	// [Info.] The following will be included in main page : div class "content"
+?>
+
+
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
+<p>Fucking home page is here</p>
---
+++ ebola/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+
+<?php // [Info.] Function prototypes
+
+	//--------------------------------------------------
+	// \brief	Get slash type (Slash or backslash)
+	// \return		Either slash or backslash
+	//__________________________________________________
+	if ( !function_exists('get_typSlash'))
+	{
+		function get_typSlash()
+		{
+			$pos = strpos(realpath('.'), '/');
+
+			if (false === $pos)
+			{
+				return '\\';
+			}
+			else
+			{
+				return '/';
+			}
+		}
+	}
+
+	//--------------------------------------------------
+	// \brief	Get root path
+	// \param	$nameRoot		|	Name of root directory
+	// \return		Root path
+	//__________________________________________________
+	if( !function_exists('get_rootPath'))
+	{
+		function get_rootPath($nameRoot)
+		{
+			$typSlash = get_typSlash();
+			$str = realpath('.');
+			$arr = explode("$typSlash", $str);
+			$ct = count($arr);
+			$i = array_search("$nameRoot", $arr);
+
+			if (false !== $i)
+			{
+				$arr = array_slice($arr, 0, $i);
+				$str = implode("$typSlash", $arr);
+			}
+			else // No corresponding root name found
+			{
+				return 'ERROR! Request wrong root name or directory does NOT exist (Path)';
+			}
+
+			return "$str" . "$typSlash" . $nameRoot;
+		}
+	}
+
+	//--------------------------------------------------
+	// \brief	Get root URL
+	// \param	$nameRoot		|	Name of root URL
+	// \return		Root URL
+	//__________________________________________________
+	if( !function_exists('get_rootURL'))
+	{
+		function get_rootURL($nameRoot)
+		{
+			$str = $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'];
+			$arr = explode('/', $str);
+			$ct = count($arr);
+			$i = array_search("$nameRoot", $arr);
+
+			if (false !== $i)
+			{
+				$arr = array_slice($arr, 0, $i);
+				$str = implode('/', $arr);
+			}
+			else // No corresponding root name found
+			{
+				return 'ERROR! Request wrong root name or directory does NOT exist (URL)';
+			}
+
+			return "$str" . '/' . $nameRoot;
+		}
+	}
+
+?>
+<?php // [Info.] Prepare variables used in (X)HTML
+
+	// Get type of slash
+	$typSlash = get_typSlash();
+	// Protocol name, usually "http://"
+	$protocol = (80 == $_SERVER["SERVER_PORT"] ? 'http://' : 'https://');
+	// Name of the root (Directory)
+	$nameRoot = 'dormnet';
+	// Script (ex. "search?q=123") WITHOUT trailing slash
+	$script = $_SERVER['SCRIPT_NAME'];
+
+	// Root path (Directory)
+	$root = get_rootPath("$nameRoot");
+	// Host (ex. "www.google.com") WITHOUT trailing slash
+	$host = "$protocol" . get_rootURL("$nameRoot");
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dorm-net 119</title>
+
+<!-- Include CSS -->
+<link href="<?php echo "$host"; ?>/[include]/[css]/main.css" rel="stylesheet" type="text/css" />
+<!--  <link href="<?php echo "$host"; ?>/[include]/[css]/DEBUG_layout.css" rel="stylesheet" type="text/css" />
+-->
+
+<!-- Include JavaScripts -->
+<script type="text/javascript" src="<?php echo "$host"; ?>/[include]/[scripts]/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="<?php echo "$host"; ?>/[include]/[scripts]/main.js"></script>
+</head>
+
+<body>
+
+<!-- @start .banner -->
+<div class="header_canvas">
+	<div class="header_wrap">
+		<div id="header_logo">
+			<a href="<?php echo "$host"; ?>/" title="Home">
+				<img alt="Home" src="<?php echo "$host"; ?>/[include]/[images]/logo.png"
+				 onMouseOver="this.src='<?php echo "$host"; ?>/[include]/[images]/logoFocus.png'"
+				 onMouseOut="this.src='<?php echo "$host"; ?>/[include]/[images]/logo.png'">
+			</a>
+		</div>
+		
+		<div id="header_nav">
+			<ul>
+				<li><div><div><a href="#">Repair</a></div></div></li>
+				<li><div><div><a href="#">Search</a></div></div></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- @end .banner --> 
+
+<!-- @start .bodyCanvas -->
+<div class="bodyCanvas"> 
+	<!-- @start .wrapper -->
+	<div class="wrapper"> 
+		<!-- @start .container -->
+		<div class="container"> 
+			<!-- @start .container content -->
+			<div class="content" id="content_leftNav">
+				
+				<div class="siderbar" id="siderbar_1">
+				<p><h3>News</h3></p>
+				<ul>
+					<li><div><a href="#">Jan 1 - CDPA games giveaway 2013</a><br/></div></li>
+					<li><div><a href="#">Jan 2 - weird monkey stole our games</a><br/></div></li>
+					<li><div><a href="#">Jan 4 - beware of ninja monkeys</a><br/></div></li>
+					<li><div><a href="#">Jan 5 - ninja monkeys hacked our website</a><br/></div></li>
+					<li><div><a href="#">Jan 5 - @$@%^!!&#$ </a><br/></div></li>
+				</ul>
+				</div>
+				
+				<div class="siderbar" id="siderbar_2">
+				<p><h3>Quick Links</h3></p>
+				<ul>
+					<li><div><a href="#">Top 100 table</a><br/></div></li>
+					<li><div><a href="#">Apply a new maintenance</a><br/></div></li>
+					<li><div><a href="#">See maintenance progress</a><br/></div></li>
+					<li><div><a href="#">Minecraft download</a><br/></div></li>
+				</ul>
+				</div>
+					
+				<div class="siderbar" id="siderbar_3">
+				<p><h3>Tutorials</h3></p>
+				<ul>
+					<li><div><a href="#">How to limit uploads</a><br/></div></li>
+					<li><div><a href="#">How to know if your network is dead</a><br/></div></li>
+					<li><div><a href="#">How to create dungeon in Minecraft</a><br/></div></li>
+					<li><div><a href="#">How to kickass</a><br/></div></li>
+					<li><div><a href="#">How to download free HD porn vid</a><br/></div></li>
+				</ul>
+				</div>
+				
+			</div>
+			<!-- @end .container content --> 
+			
+			<!-- @start .container contents -->
+			<div class="content" id="content_main">
+				<?php // [Info.] Require contents in other files
+
+					/*	Insert "[include]" into current script path
+						ex. '/dormnet/rescue/1.php' ->
+							'/dormnet/[include]/rescue/1.php'
+
+						So you must put files which have exactly same name as front pages
+					*/
+					$arr = explode('/', "$script");
+					$ct = count($arr);
+					$i = array_search("$nameRoot", $arr);
+
+					$arr = array_slice($arr, $i, $ct - $i);
+
+					$i = array_search("$nameRoot", $arr);
+					array_splice($arr, $i + 1, 0, array('[include]')); // Insert "[include]"
+					$str = implode('/', $arr);
+
+					require_once dirname("$root") . '/' . $str;
+				?>
+			</div>
+			<!-- @end .container contents --> 
+			
+		</div>
+		<!-- @end .container --> 
+		
+	</div>
+	<!-- @end .wrapper --> 
+
+	<div class="img_hr" align="middle">
+		<img src="<?php echo "$host"; ?>/[include]/[images]/hr_gray.png">
+	</div>
+	
+	<!-- @start .footer_wrap -->
+	<div class="footer_wrap">
+		<div class="footer">
+			<a href="#">Home</a> | <a href="#">Products</a> | <a href="#">Services</a> | <a href="#">About Us</a> | <a href="#">Contact Us</a> | <a href="#">Site Map</a> | <a href="#">Privacy</a><br />
+			<br />
+			Copyright 2012 NSYSU-CDPA. All Rights Reserved. <img src="" width="1" /> 
+		</div>
+	</div>
+	<!-- @end .footer_wrap --> 
+</div>
+<!-- @end .bodyCanvas -->
+
+</body>
+</html>
