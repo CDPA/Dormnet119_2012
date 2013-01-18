cfb92bd5c835f2f3a00f31122985993722c13232
莊鈺婷 <abc50931@hotmail.com>
2013-01-19 01:16:03 +0800

    改路徑

--- ebola/[include]/repair/guide.php
+++ ebola/[include]/repair/guide.php
@@ -5,10 +5,9 @@
 
 <center>
 	<div>			
-		<BUTTON onclick="self.location.href='p1.php'">報修</BUTTON>
-		<BUTTON onclick="self.location.href='p2.php'">前置檢查</BUTTON>
-		<BUTTON onclick="self.location.href='p3.php'">填報修單</BUTTON>
-		<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
+		<BUTTON onclick="self.location.href='p1.php'">前置檢查</BUTTON>
+		<BUTTON onclick="self.location.href='p2.php'">填報修單</BUTTON>
+		<BUTTON onclick="self.location.href='p3.php'">改報修單</BUTTON>
 	</div>
 	<hr>
 	<div class=out1 style='text-align:center; line-height:450px'>
