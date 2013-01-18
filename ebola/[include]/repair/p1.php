c24a0e2e6f1ad4cb369c532b0771bc474ee65305
莊鈺婷 <abc50931@hotmail.com>
2013-01-18 23:04:07 +0800

    啦啦啦

--- ebola/[include]/repair/p1.php
+++ ebola/[include]/repair/p1.php
@@ -3,16 +3,16 @@
 ?>
 
 
-		<center>
-			<div>			
-				<BUTTON onclick="self.location.href='p1.php'">報修</BUTTON>
-				<BUTTON onclick="self.location.href='p2.php'">前置檢查</BUTTON>
-				<BUTTON onclick="self.location.href='p3.php'">填報修單</BUTTON>
-				<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
-			</div>
-			<hr>
-			<div class=out1 style='text-align:center; line-height:450px'>
-				<BUTTON onclick="self.location.href='p2.php'">填報修單</BUTTON>
-				<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
-			</div>
-       	<center/>
+<center>
+	<div>			
+		<BUTTON onclick="self.location.href='p1.php'">報修</BUTTON>
+		<BUTTON onclick="self.location.href='p2.php'">前置檢查</BUTTON>
+		<BUTTON onclick="self.location.href='p3.php'">填報修單</BUTTON>
+		<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
+	</div>
+	<hr>
+	<div class=out1 style='text-align:center; line-height:450px'>
+		<BUTTON onclick="self.location.href='p2.php'">填報修單</BUTTON>
+		<BUTTON onclick="self.location.href='p4.php'">改報修單</BUTTON>
+	</div>
+<center/>
