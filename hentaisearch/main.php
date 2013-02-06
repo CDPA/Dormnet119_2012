f688f3f09711dd25228f5c9910b15fa867763592
Windows (R) 3.1 <alastorid@gmail.com>
2013-02-06 17:27:06 +0800

    ㄜ他逼我SYNC我也沒辦法 沒寫完說

---
+++ hentaisearch/main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html><title>main()</title>
+<script language="JavaScript" src="jquery.js" type="text/javascript"></script>
+<script language="JavaScript" src="main.js" type="text/javascript"></script>
+<link href="main.css" rel="stylesheet" type="text/css" />
+
+<body>
+
+<div class="list">
+<div class="box-image"></div>
+<div class="box-content"></div>
+<p id='l'>LIST</p></div>
+
+<div class="list">
+<div class="box-image"></div>
+<div class="box-content"></div>
+<p id='l'>LIST</p></div>
+
+<div class="list">
+<div class="box-image"></div>
+<div class="box-content"></div>
+<p id='l'>LIST</p></div>
+
+</body>
