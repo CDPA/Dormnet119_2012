67d2a4801ed3cf3bd1277aee60aed7de241176a8
Windows (R) 3.1 <alastorid@gmail.com>
2013-02-09 09:10:19 +0800

    資料備份不小心也傳上去了0.0

--- finished_search_119/[include]/search/guide.php
+++
@@ -1,4 +0,0 @@
-﻿
-<?php
-require_once(realpath('..') .  '\[include]\search\main.php');
-?>
--- wired_php_infects_jQ/[include]/support/teach_map.php
+++
@@ -1,6 +0,0 @@
-﻿
-<?php	// [Info.] The following code will be included in main page : div class "content"
-?>
-		<form action="guide.php" >
-			<input type="submit" value="BACK">
-		</form>
